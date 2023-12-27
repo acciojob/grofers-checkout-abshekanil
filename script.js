edd25b5b87ddf09b133bc0ba1b04dc2da197ff9c
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2023-12-27 12:09:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let table = document.getElementsByTagName('table')[0];
+	let newRow = table.insertRow(table.rows.length);
+
+	let newTh = document.createElement('th');
+	newTh.textContent = 'Total Price';
+
+	newRow.appendChild(newTh);
   
 };
 
